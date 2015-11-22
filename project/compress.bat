1ef9de898eea55d91df3dc92c48221195e7fdc61
carborane3 <pi3_1415926535897932@yahoo.co.jp>
2015-11-23 03:47:36 +0900

    Add

---
+++ project/compress.bat
@@ -0,0 +1,5 @@
+rem **** For Windows installed Node.js and UglifyJS ****
+cmd /c "uglifyjs -c -m -o ../flmmlonhtml5.js flmmlonhtml5-raw.js"
+cmd /c "uglifyjs -c -m -o ../flmmlworker.js flmmlworker-raw.js"
+cmd /c "uglifyjs -c -m -o ../flmmlplayer.js flmmlplayer-raw.js"
+pause
