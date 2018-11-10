a6f11e2e758a2cf4fc5972359908c27190b386f4
carborane3 <pi3_1415926535897932@yahoo.co.jp>
2018-11-10 14:16:00 +0900

    #16 対応

--- src/flmmlonhtml5-raw.js
+++ src/flmmlonhtml5-raw.js
@@ -1,4 +1,4 @@
-﻿/*! FlMMLonHTML5 v1.1.2 | (c) 2018, carborane3 | BSD-3-Clause | https://github.com/carborane3/FlMMLonHTML5 */
+﻿/*! flmml-on-html5 v1.2.0 | (c) 2018, argentum384 | BSD-3-Clause | https://github.com/argentum384/flmml-on-html5 */
 
 "use strict";
 
