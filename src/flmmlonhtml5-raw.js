f5c56946616e669512030efee24a4b1357954e16
carborane3 <pi3_1415926535897932@yahoo.co.jp>
2018-11-10 12:24:11 +0900

    #21 対応

--- src/flmmlonhtml5-raw.js
+++ src/flmmlonhtml5-raw.js
@@ -3,23 +3,6 @@
 "use strict";
 
 var FlMMLonHTML5 = function () {
-    var BUFFER_SIZE = 8192;
-
-    var COM_BOOT      =  1, // Main->Worker
-        COM_PLAY      =  2, // Main->Worker
-        COM_STOP      =  3, // Main->Worker
-        COM_PAUSE     =  4, // Main->Worker
-        COM_BUFFER    =  5, // Main->Worker->Main
-        COM_COMPCOMP  =  6, // Worker->Main
-        COM_BUFRING   =  7, // Worker->Main
-        COM_COMPLETE  =  8, // Worker->Main
-        COM_SYNCINFO  =  9, // Main->Worker->Main
-        COM_PLAYSOUND = 10, // Worker->Main
-        COM_STOPSOUND = 11, // Worker->Main->Worker
-        COM_DEBUG     = 12; // Worker->Main
-
-    var emptyBuffer = new Float32Array(BUFFER_SIZE);
-
     var divDebug;
 
     function debug(str) {
@@ -60,50 +43,70 @@ var FlMMLonHTML5 = function () {
         this.events = {};
         
         worker.postMessage({
-            type: COM_BOOT,
+            type: FlMMLonHTML5.COM_BOOT,
             sampleRate: FlMMLonHTML5.audioCtx.sampleRate,
-            bufferSize: BUFFER_SIZE
+            bufferSize: FlMMLonHTML5.BUFFER_SIZE
         });
         this.setInfoInterval(125);
     }
 
+    // static
+    extend(FlMMLonHTML5, {
+        BUFFER_SIZE: 8192,
+
+        COM_BOOT     :  1, // Main->Worker
+        COM_PLAY     :  2, // Main->Worker
+        COM_STOP     :  3, // Main->Worker
+        COM_PAUSE    :  4, // Main->Worker
+        COM_BUFFER   :  5, // Main->Worker->Main
+        COM_COMPCOMP :  6, // Worker->Main
+        COM_BUFRING  :  7, // Worker->Main
+        COM_COMPLETE :  8, // Worker->Main
+        COM_SYNCINFO :  9, // Main->Worker->Main
+        COM_PLAYSOUND: 10, // Worker->Main
+        COM_STOPSOUND: 11, // Worker->Main->Worker
+        COM_DEBUG    : 12  // Worker->Main
+    });
+    FlMMLonHTML5.emptyBuffer = new Float32Array(FlMMLonHTML5.BUFFER_SIZE);
+
+    // not static
     extend(FlMMLonHTML5.prototype, {
         onMessage: function (e) {
             var data = e.data,
                 type = data.type;
 
-            //console.log("Main received " + type);
+            // console.log("Main received " + Object.keys(FlMMLonHTML5).filter(k => k.indexOf("COM_") === 0 && FlMMLonHTML5[k] === type));
             switch (type) {
-                case COM_BUFFER:
+                case FlMMLonHTML5.COM_BUFFER:
                     this.buffer = data.buffer;
                     this.bufferReady = true;
                     break;
-                case COM_COMPCOMP:
+                case FlMMLonHTML5.COM_COMPCOMP:
                     extend(this, data.info);
                     this.oncompilecomplete && this.oncompilecomplete();
                     this.trigger("compilecomplete");
                     break;
-                case COM_BUFRING:
+                case FlMMLonHTML5.COM_BUFRING:
                     this.onbuffering && this.onbuffering(data);
                     this.trigger("buffering", data);
                     break;
-                case COM_COMPLETE:
+                case FlMMLonHTML5.COM_COMPLETE:
                     this.oncomplete && this.oncomplete();
                     this.trigger("complete");
                     break;
-                case COM_SYNCINFO:
+                case FlMMLonHTML5.COM_SYNCINFO:
                     extend(this, data.info);
                     this.onsyncinfo && this.onsyncinfo();
                     this.trigger("syncinfo");
                     break;
-                case COM_PLAYSOUND:
+                case FlMMLonHTML5.COM_PLAYSOUND:
                     this.playSound();
                     break;
-                case COM_STOPSOUND:
+                case FlMMLonHTML5.COM_STOPSOUND:
                     this.stopSound(data.isFlushBuf);
-                    this.worker.postMessage({ type: COM_STOPSOUND });
+                    this.worker.postMessage({ type: FlMMLonHTML5.COM_STOPSOUND });
                     break;
-                case COM_DEBUG:
+                case FlMMLonHTML5.COM_DEBUG:
                     debug(data.str);
             }
         },
@@ -117,7 +120,7 @@ var FlMMLonHTML5 = function () {
             gain.gain.value = this.volume / 127.0;
             gain.connect(audioCtx.destination);
 
-            this.scrProc = audioCtx.createScriptProcessor(BUFFER_SIZE, 1, 2);
+            this.scrProc = audioCtx.createScriptProcessor(FlMMLonHTML5.BUFFER_SIZE, 1, 2);
             this.scrProc.addEventListener("audioprocess", this.onAudioProcessBinded);
             this.scrProc.connect(this.gain);
 
@@ -144,11 +147,11 @@ var FlMMLonHTML5 = function () {
                 outBuf.getChannelData(0).set(this.buffer[0]);
                 outBuf.getChannelData(1).set(this.buffer[1]);
                 this.bufferReady = false;
-                this.worker.postMessage({ type: COM_BUFFER, retBuf: this.buffer }, [this.buffer[0].buffer, this.buffer[1].buffer]);
+                this.worker.postMessage({ type: FlMMLonHTML5.COM_BUFFER, retBuf: this.buffer }, [this.buffer[0].buffer, this.buffer[1].buffer]);
             } else {
-                outBuf.getChannelData(0).set(emptyBuffer);
-                outBuf.getChannelData(1).set(emptyBuffer);
-                this.worker.postMessage({ type: COM_BUFFER, retBuf: null });
+                outBuf.getChannelData(0).set(FlMMLonHTML5.emptyBuffer);
+                outBuf.getChannelData(1).set(FlMMLonHTML5.emptyBuffer);
+                this.worker.postMessage({ type: FlMMLonHTML5.COM_BUFFER, retBuf: null });
             }
         },
 
@@ -164,15 +167,15 @@ var FlMMLonHTML5 = function () {
 
 
         play: function (mml) {
-            this.worker.postMessage({ type: COM_PLAY, mml: mml });
+            this.worker.postMessage({ type: FlMMLonHTML5.COM_PLAY, mml: mml });
         },
 
         stop: function () {
-            this.worker.postMessage({ type: COM_STOP });
+            this.worker.postMessage({ type: FlMMLonHTML5.COM_STOP });
         },
 
         pause: function () {
-            this.worker.postMessage({ type: COM_PAUSE });
+            this.worker.postMessage({ type: FlMMLonHTML5.COM_PAUSE });
         },
 
         setMasterVolume: function (volume) {
@@ -229,11 +232,11 @@ var FlMMLonHTML5 = function () {
         },
 
         setInfoInterval: function (interval) {
-            this.worker.postMessage({ type: COM_SYNCINFO, interval: interval });
+            this.worker.postMessage({ type: FlMMLonHTML5.COM_SYNCINFO, interval: interval });
         },
 
         syncInfo: function () {
-            this.worker.postMessage({ type: COM_SYNCINFO, interval: null });
+            this.worker.postMessage({ type: FlMMLonHTML5.COM_SYNCINFO, interval: null });
         },
 
         addEventListener: function (type, listener) {
