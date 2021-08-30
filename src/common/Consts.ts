aca3ac0cd046720c17eab176a9e0d84ab86aa858
argentum384 <argentum384@gmail.com>
2021-08-30 19:16:08 +0900

    微修正

--- src/common/Consts.ts
+++ src/common/Consts.ts
@@ -1,8 +1,8 @@
 export const SEQUENCER_SAMPLE_RATE = 44100;
 
 // 以下2点を満たすために 640 の倍数にする
-// - AudioWorklet のバッファサイズ 128 の倍数にする
-// - レート 44100 Hz → 48000, 96000, ... Hz 変換で誤差出ないようにする
+// * AudioWorklet のバッファサイズ 128 の倍数にする
+// * レート 44100 Hz → 48000, 96000, ... Hz 変換で誤差出ないようにする
 export const AUDIO_BUFFER_SIZE = 7680;
 
 export const MsgTypes = {
